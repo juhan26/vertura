39509f17f538fbc58997ec7669da79c50ad032dd
Juhanda Azril Kamal <cukjuhan@gmail.com>
2025-05-10 17:11:41 +0800

    new layout title and description

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
+  title: 'Vertura',
+  description: 'Wear the culture, rise up with a twist',
   generator: 'v0.dev',
 }
 
